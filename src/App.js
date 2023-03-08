4ea7da4ce2e5686569c3d4f5be2e5023e380a075
seni95 <ghkdtpdus95@naver.com>
2023-03-08 21:38:53 +0900

    ground effect by Three.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 
+import * as THREE from 'three';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { Suspense } from 'react';
+import Ground from './components/Apple/Ground';
+
+const Apple = (props)=> {
+  return (
+    <>
+    {/* orbitcontrols 3js를 돕는 도우미 궤도 컨트롤의 대상인 고정 지점 주위로 카메라를 이동 할 수 있습니다.*/}
+    <OrbitControls target={[0,0.35,0]} maxPolarAngle={1.45}></OrbitControls>
+     {/* 카메라 */}
+    <PerspectiveCamera makeDefault fov={50} position={[3,2,5]}></PerspectiveCamera>
+
+    {/* 컬러를 정의하는 함수 - 0,0,0은 검정색임 */}
+    <color args={[0,0,0]} attach="background"></color>
+
+    <spotLight
+    color={[1,0.25,0.7]}
+    intensity = {1.5}
+    angle={0.6}
+    penumbra={0.5}
+    position={[5,5,0]}
+    castShadow
+    shadow-bias={-0.0001}
+    ></spotLight>
+
+<spotLight
+    color={[1,0.25,0.7]}
+    intensity = {1.5}
+    angle={0.6}
+    penumbra={0.5}
+    position={[5,5,0]}
+    castShadow
+    shadow-bias={-0.0001}
+    ></spotLight>
+   <Ground></Ground>
+    </>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense>
+      <Canvas>
+        <Apple></Apple>
+      </Canvas>
+    </Suspense>
   );
 }
 
